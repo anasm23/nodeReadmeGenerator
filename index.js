f353592aa774406a6a448b1b37783e4a45f0aba5
anasm23 <muhammadanas112@gmail.com>
2021-11-08 17:57:06 -0500

    created inquirer prompt, and declared node dependencies.

---
+++ index.js
@@ -0,0 +1,9 @@
+// Creating node dependencies
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+//inquirer prompt which will prompt readme information to fill out
+inquirer.prompt([{
+
+}]).then({
+});
