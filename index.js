8376def1af902b1efbe582c716a87ec79a591e96
anasm23 <muhammadanas112@gmail.com>
2021-11-21 01:48:41 -0500

    fixed ES6 syntax

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Creating node dependencies
-var inquirer = require("inquirer");
-var fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 
 //inquirer prompt which will prompt readme information to fill out
 inquirer.prompt([
